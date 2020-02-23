d31e3caf9b8c0a785132ae16dfd697bec85243f4
iamsusiep <sp3581@columbia.edu>
2020-02-22 22:15:16 -0500

    update on code from vm

--- sanity_check.py
+++ sanity_check.py
@@ -5,16 +5,26 @@ with open('/home/suji/spring20/Large-Scale-VRD/datasets/large_scale_VRD/Visual_G
     for line in obj_categories:
         _object_categories.add(line[:-1])
 
+train_fn = '/home/suji/spring20/vilbert_beta/data/VCR/orig/train.jsonl'
 val_fn= '/home/suji/spring20/vilbert_beta/data/VCR/orig/val.jsonl'
 entries = []
 with json_lines.open(val_fn) as reader:
     for obj in reader:
         entries.append(obj)
+with json_lines.open(train_fn) as reader:
+    for obj in reader:
+        entries.append(obj)
+
+
 vcr_obj = set()
 for entry in entries:
     for x in entry["objects"]:
-	    vcr_obj.append(x)
-print("not in category")
-for x in in vcr_obj:
+	    vcr_obj.add(x)
+m = {'hairdrier': 'hair drier', 'pottedplant': 'potted plant', 'trafficlight': 'traffic light', 'teddybear':'teddy bear', 'baseballbat': 'baseball bat', 'baseballglove': 'baseball glove', 'tennisracket': 'tennis racket', 'diningtable': 'dining table', 'parkingmeter': 'parking meter', 'sportsball': 'sports ball', 'wineglass': 'wine glass', 'hotdog': 'hot dog', 'stopsign': 'stop sign', 'firehydrant': 'fire hydrant'}
+print("total categories in vcr:{}\nnot in category".format(len(vcr_obj)))
+for x in vcr_obj:
     if x not in _object_categories:
+        if x in m.keys():
+            x = m[x]
+            continue
         print(x)
